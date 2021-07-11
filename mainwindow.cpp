2dad26ce68194e7a81e0cfb270ae2665b86b0680
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2021-07-11 15:15:25 +0200

    adding Buttons & Text Fields

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,10 +6,47 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-}
+    this->setCentralWidget(ui->groupBox);
+    ui->textEdit->setFontPointSize(14);
+    ui->textBrowser->setFontPointSize(14);
+    ui->textEdit->setLineWrapMode(QTextEdit::LineWrapMode(0));
+   }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+void MainWindow::on_actionNew_triggered()
+{
+    inputFieldFile.clear();
+    ui->textEdit->setText(QString());
+    ui->textBrowser->clear();
+    ui->textEdit->setFontPointSize(14);
+    ui->textBrowser->setFontPointSize(14);
+
+}
+
+
+void MainWindow::on_actionImport_triggered()
+{
+     QString fileName = QFileDialog::getOpenFileName(this, "Open The File");
+       QFile file(fileName);
+       inputFieldFile = fileName;
+       if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+             QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+             return;
+         }
+         setWindowTitle(fileName);
+         QTextStream in(&file);
+         QString text = in.readAll();
+         ui->textEdit->setText(text);
+         file.close();
+         ui->textEdit->selectAll();
+         ui->textEdit->setFontPointSize(14);
+
+}
+
+
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +19,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionImport_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QString inputFieldFile = "";
 };
 #endif // MAINWINDOW_H
