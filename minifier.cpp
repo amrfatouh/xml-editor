fe19e3e8cb9a260a06649daa429122ac6dccb66e
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2021-07-13 09:08:40 +0200

    adding test cases to minifier.cpp

--- minifier.cpp
+++ minifier.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <algorithm>
 #include <sstream>
+#include <iostream>
 using namespace std;
 string minify(string y)
 {
@@ -47,4 +48,69 @@ string minify(string y)
     }
     return y;
 
+}
+
+void testMinify()
+{
+  string s;
+
+  cout << "new line between opening and closing tags\n";
+  s = "<body>\n</body>";
+  cout << "string: " + s + "\n";
+  cout << "expected output: <body></body>" << endl;
+  cout << "actual output: " << minify(s) << "\n\n";
+
+  cout << "space between opening and closing tags\n";
+  s = "<body> </body>";
+  cout << "string: " + s + "\n";
+  cout << "expected output: <body></body>" << endl;
+  cout << "actual output: " << minify(s) << "\n\n";
+
+  cout << "space inside opening tag\n";
+  s = "< body ></body>";
+  cout << "string: " + s + "\n";
+  cout << "expected output: <body></body>" << endl;
+  cout << "actual output: " << minify(s) << "\n\n";
+
+  cout << "spaces inside closing tags\n";
+  s = "<body>< / body >";
+  cout << "string: " + s + "\n";
+  cout << "expected output: <body></body>" << endl;
+  cout << "actual output: " << minify(s) << "\n\n";
+
+  cout << "new lines and inner tags\n";
+  s = "<body>\n<div></div>\n</body>";
+  cout << "string: " + s + "\n";
+  cout << "expected output: <body><div></div></body>" << endl;
+  cout << "actual output: " << minify(s) << "\n\n";
+
+  cout << "spaces and text\n";
+  s = "<body> hello world </body>";
+  cout << "string: " + s + "\n";
+  cout << "expected output: <body>hello world</body>" << endl;
+  cout << "actual output: " << minify(s) << "\n\n";
+
+  cout << "five spaces in text\n";
+  s = "<body>hello     world</body>";
+  cout << "string: " + s + "\n";
+  cout << "expected output: <body>hello     world</body>" << endl;
+  cout << "actual output: " << minify(s) << "\n\n";
+
+  cout << "spaces in comment\n";
+  s = "<!--   hello   -->";
+  cout << "string: " + s + "\n";
+  cout << "expected output: <!-- hello -->" << endl;
+  cout << "actual output: " << minify(s) << "\n\n";
+
+  cout << "space between inner tags\n";
+  s = "<body><div></div> <div></div></body>";
+  cout << "string: " + s + "\n";
+  cout << "expected output: <body><div></div><div></div></body>" << endl;
+  cout << "actual   output: " << minify(s) << "\n\n";
+
+  cout << "tabs and new lines\n";
+  s = "<body>\n\t<div>\n\t</div>\n</body>";
+  cout << "string: " + s + "\n";
+  cout << "expected output: <body><div></div></body>" << endl;
+  cout << "actual   output: " << minify(s) << "\n\n";
 }
